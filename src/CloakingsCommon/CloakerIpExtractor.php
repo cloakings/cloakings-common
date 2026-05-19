187be9ed8c1543c5a22151c12a864faaddcf0dd1
Vova Luchaninov <vladimir.luchaninov@gmail.com>
2026-05-19 12:31:40 +0200

    support symfony 8

--- src/CloakingsCommon/CloakerIpExtractor.php
+++ src/CloakingsCommon/CloakerIpExtractor.php
@@ -6,29 +6,29 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CloakerIpExtractor
 {
-    private const DEFAULT_IP = '127.0.0.1';
+    private const string DEFAULT_IP = '127.0.0.1';
 
     public function getIp(Request $request, CloakerIpExtractorModeEnum $mode = CloakerIpExtractorModeEnum::Aggressive): string
     {
-        if ($mode === CloakerIpExtractorModeEnum::Simple) {
-            return $request->getClientIp() ?? self::DEFAULT_IP;
+        return match ($mode) {
+            CloakerIpExtractorModeEnum::Simple => $request->getClientIp() ?? self::DEFAULT_IP,
+            CloakerIpExtractorModeEnum::Aggressive => $this->getAggressive($request),
+        };
+    }
+
+    private function getAggressive(Request $request): string
+    {
+        $ip = $request->headers->get('cf-connecting-ip', '');
+        if ($ip !== '') {
+            return explode(',', $ip, 2)[0];
         }
 
-        if ($mode === CloakerIpExtractorModeEnum::Aggressive) {
-            $ip = $request->headers->get('cf-connecting-ip', '');
-            if ($ip !== '') {
-                return explode(',', $ip, 2)[0];
-            }
+        $trustedProxies = Request::getTrustedProxies();
+        $trustedHeaderSet = Request::getTrustedHeaderSet();
+        Request::setTrustedProxies(['0.0.0.0/0'], -1);
+        $ip = $request->getClientIp() ?? self::DEFAULT_IP;
+        Request::setTrustedProxies($trustedProxies, $trustedHeaderSet);
 
-            $trustedProxies = Request::getTrustedProxies();
-            $trustedHeaderSet = Request::getTrustedHeaderSet();
-            Request::setTrustedProxies(['0.0.0.0/0'], -1);
-            $ip = $request->getClientIp() ?? self::DEFAULT_IP;
-            Request::setTrustedProxies($trustedProxies, $trustedHeaderSet);
-
-            return $ip;
-        }
-
-        return $request->getClientIp() ?? self::DEFAULT_IP;
+        return $ip;
     }
 }
